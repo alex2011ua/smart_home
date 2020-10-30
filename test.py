44bb4a1648978cd39264ffeb17c3e4de74d46999
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-10-30 15:53:43 +0200

    to raspberry

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import serial
 
 import time
 
-ser = serial.Serial("/dev/ttyACM0", 9600)  #change ACM number as found from ls /dev/tty/ACM*
+ser = serial.Serial("/dev/ttyUSB0", 9600)  #change ACM number as found from ls /dev/tty/ACM*
 ser.baudrate = 9600
 
 
