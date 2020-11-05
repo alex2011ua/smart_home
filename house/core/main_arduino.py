446d4c992f4cc3770f0dcd28683354ead04e586a
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-11-05 11:57:23 +0200

    to raspberry

--- house/core/main_arduino.py
+++ house/core/main_arduino.py
@@ -24,17 +24,12 @@ def read_ser():
                         9600)  # change ACM number as found from ls /dev/tty/ACM*
     ser.baudrate = 9600
     data = ser.read(ser.inWaiting())
-    print(data)
     ser.write(b'2')
     s = ser.readline().strip().decode()
-    print(s)
     if s == "get data":
-        print('get dada - true')
         time.sleep(1)
         read_ser = ser.read(ser.inWaiting())
-        print(read_ser)
         a = read_ser.decode().strip().split(':')
-        context = {'Humidity': a[1], 'Temperature': a[3]}
+        context = {'Humidity': int(a[1][0:-3]), 'Temperature': int(a[3][0:-3])}
         return context
-    print('get dada - false')
     ser.close()
