029c9bcec4f7f94aa27ccc134b9172daec5a1f87
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-11-05 09:20:38 +0200

    to raspberry

--- house/celery.py
+++ house/celery.py
@@ -13,28 +13,37 @@ cellery_app.config_from_object('django.conf:settings', namespace='CELERY')
 cellery_app.autodiscover_tasks()
 
 from house.core.tasks import restart_cam_task, weather_task, arduino_task
+from celery.exceptions import SoftTimeLimitExceeded
 
 # запуск рестарта камер
-@cellery_app.on_after_configure.connect
+@cellery_app.on_after_configure.connect(time_limit=20)
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(
-        crontab(minute = 0,
-                hour = '6,15,21'),
-        restart_cam_task.s(),
-        name = 'Restart cam')
-
+    try:
+        sender.add_periodic_task(
+            crontab(minute = 0,
+                    hour = '6,15,21'),
+            restart_cam_task.s(),
+            name = 'Restart cam')
+    except SoftTimeLimitExceeded as err:
+        print(err)
 # запуск обновления ино о погоде
-@cellery_app.on_after_configure.connect
+@cellery_app.on_after_configure.connect(time_limit=20)
 def setup_periodic_tasks_weather(sender, **kwargs):
-    sender.add_periodic_task(
-        crontab(minute=0, hour=1),
-        weather_task.s(),
-        name = 'Weather')
+    try:
+        sender.add_periodic_task(
+            crontab(minute=0, hour=1),
+            weather_task.s(),
+            name = 'Weather')
+    except SoftTimeLimitExceeded as err:
+        print(err)
 
 # запуск обновления ино arduino
-@cellery_app.on_after_configure.connect
+@cellery_app.on_after_configure.connect(time_limit=60)
 def setup_periodic_task_arduino(sender, **kwargs):
-    sender.add_periodic_task(
-        crontab(minute=5),
-        arduino_task.s(),
-        name = 'arduino')
+    try:
+        sender.add_periodic_task(
+            crontab(minute=5),
+            arduino_task.s(),
+            name = 'arduino')
+    except SoftTimeLimitExceeded as err:
+        print(err)
