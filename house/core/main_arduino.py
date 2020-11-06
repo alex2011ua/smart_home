1f03b48caef7512bff184b1027d3d19bf90bb3d9
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-11-06 09:26:14 +0200

    to raspberry

--- house/core/main_arduino.py
+++ house/core/main_arduino.py
@@ -8,11 +8,14 @@ def restart_cam():
                         9600)  # change ACM number as found from ls /dev/tty/ACM*
     ser.baudrate = 9600
     data = ser.read(ser.inWaiting())
+    time.sleep(0.5)
     ser.write(b'0')
+    time.sleep(0.5)
     if ser.readline().strip().decode() != "rele off":
         status = False
     time.sleep(10)
     ser.write(b'1')
+    time.sleep(0.5)
     if ser.readline().strip().decode() != "rele on":
         status = False
     ser.close()
@@ -24,10 +27,11 @@ def read_ser():
                         9600)  # change ACM number as found from ls /dev/tty/ACM*
     ser.baudrate = 9600
     data = ser.read(ser.inWaiting())
+    time.sleep(0.5)
     ser.write(b'2')
     s = ser.readline().strip().decode()
     if s == "get data":
-        time.sleep(1)
+        time.sleep(0.5)
         read_ser = ser.read(ser.inWaiting())
         a = read_ser.decode().strip().split(':')
         context = {'Humidity': int(a[1][0:-3]), 'Temperature': int(a[3][0:-3])}
