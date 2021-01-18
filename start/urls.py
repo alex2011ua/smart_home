ffd3fd4b82c3e9f4a99bc00857008d901c1a9942
AlexUA <60536573+alex2011ua@users.noreply.github.com>
2021-01-18 14:35:09 +0200

    add viber   bot

--- start/urls.py
+++ start/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from django.contrib.auth import views
 from . import start_views
 import os
-
+from myviberbot.views import trx_bot
 
 token = os.getenv('TOKEN', os.environ.get('TOKEN'))
 
@@ -11,5 +11,6 @@ urlpatterns = [
     path('', start_views.index, name='index'),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
-    path('{}/'.format(token), start_views.get_bot_message, name='get_bot_message')
+    path('{}/'.format(token), start_views.get_bot_message, name='get_bot_message'),
+    path('webhook2020/', trx_bot),
 ]
