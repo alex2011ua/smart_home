9a00019423e2c8ee585f3fa8f8dea8d2cd21cf1e
AlexUA <60536573+alex2011ua@users.noreply.github.com>
2021-01-19 12:30:28 +0200

    add viber   bot

--- myviberbot/viber_bot.py
+++ myviberbot/viber_bot.py
@@ -2,20 +2,17 @@ from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
 import os
 from dotenv import load_dotenv
+from house.core.Telegram import bot
+
 from viberbot.api.messages import (TextMessage, PictureMessage)
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
-
+token = os.getenv('viber_token', os.environ.get('viber_token'))
 bot_configuration = BotConfiguration(
 	name='AlexUA',
 	avatar='http://alexua.pp.ua/static/start/favico.jpg',
-	auth_token= os.getenv('viber_token', os.environ.get('viber_token'))
+	auth_token=token
 )
 viber = Api(bot_configuration)
-
-# creation of text message
-text_message = TextMessage(text="sample text message!")
-
-# creation of picture message
-picture_message = PictureMessage(text="Check this", media="http://site.com/img.jpg")
+bot.send_message('create viber,  '+token)
