a975e148511f693219a393159d81ace533219fc8
Aleksey Kovalenko <alex2011ua@gmail.com>
2021-03-26 10:36:38 +0200

    refactor

--- start/start_views.py
+++ start/start_views.py
@@ -6,4 +6,16 @@ from django.views import View
 
 
 
+class IndexView(View):
+    @staticmethod
+    def get(request):
 
+        context = {
+            's_yers': [2016],
+            'po_yers': [2018],
+            'price_ot': 10000,
+            'price_do': 10500,
+            'type': ['1', '4', '6'],
+            'gearbox': ['2', '3']
+        }
+        return render(request, "start/index.html", context)
--- start/urls.py
+++ start/urls.py
@@ -1,15 +1,14 @@
 from django.urls import include, path
 from django.contrib.auth import views
-
 from myviberbot.views import trx_bot
-
+from .start_views import IndexView
 
 
 
 urlpatterns = [
     path('house/', include('house.urls')),
     path('avto/', include('selection_avto.urls')),
-# path('', start_views.IndexView.as_view(), name='index'),
+    path('', IndexView.as_view(), name='index'),
 
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
