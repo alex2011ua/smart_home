40f8e37a08c568c0ce250d02262d85c0eba4b440
Aleksey Kovalenko <alex2011ua@gmail.com>
2021-03-30 17:43:09 +0300

    poliv

--- house/core/poliv_view.py
+++ house/core/poliv_view.py
@@ -1,8 +1,11 @@
 from django.views import View
 from django.shortcuts import render
+from .models import Setting
+
 
 class  Poliv(View):
     @staticmethod
     def get(request):
-        context = {}
+        poliv = Setting.objects.get(controller_name='poliv')
+        context = {'poliv': poliv}
         return render(request, 'core/poliv_index.html', context)
