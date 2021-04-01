8c5a4e8412c7eed6bbdc4135f43188e01c4b521a
Aleksey Kovalenko <alex2011ua@gmail.com>
2021-04-01 17:01:43 +0300

    add js

--- house/core/poliv_view.py
+++ house/core/poliv_view.py
@@ -1,11 +1,47 @@
 from django.views import View
 from django.shortcuts import render
 from .models import Setting
+from django.core.exceptions import ObjectDoesNotExist
 
 
-class  Poliv(View):
+class Poliv(View):
     @staticmethod
     def get(request):
+        poliv_all = get_status_poliv()
         poliv = Setting.objects.get(controller_name='poliv')
         context = {'poliv': poliv}
+
         return render(request, 'core/poliv_index.html', context)
+
+    @staticmethod
+    def post(request):
+        print(request.POST)  # print all url params
+
+def get_status_poliv():
+    try:
+        poliv_elki = Setting.objects.get(controller_name='poliv_elki')
+        poliv_garaz = Setting.objects.get(controller_name='poliv_garaz')
+        poliv_pesochnica = Setting.objects.get(controller_name='poliv_pesochnica')
+        poliv_teplica = Setting.objects.get(controller_name='poliv_teplica')
+        poliv_sad = Setting.objects.get(controller_name='poliv_sad')
+        poliv_strawberry = Setting.objects.get(controller_name='poliv_strawberry')
+    except ObjectDoesNotExist:
+        poliv_elki = Setting.objects.create(controller_name='poliv_elki', label='выключен', value=0)
+        poliv_garaz = Setting.objects.create(controller_name='poliv_garaz', label='выключен', value=0)
+        poliv_pesochnica = Setting.objects.create(controller_name='poliv_pesochnica', label='выключен', value=0)
+        poliv_teplica = Setting.objects.create(controller_name='poliv_teplica', label='выключен', value=0)
+        poliv_sad = Setting.objects.create(controller_name='poliv_sad', label='выключен', value=0)
+        poliv_strawberry = Setting.objects.create(controller_name='poliv_strawberry', label='выключен', value=0)
+    poliv_all = []
+    list(map(lambda x: poliv_all.append(x),
+             [
+                 poliv_elki,
+                 poliv_garaz,
+                 poliv_pesochnica,
+                 poliv_teplica,
+                 poliv_sad,
+                 poliv_strawberry,
+             ]
+             ))
+    return poliv_all
+
--- house/core/static/js/polivScript.js
+++ house/core/static/js/polivScript.js
@@ -1,6 +1,30 @@
-sad.addEventListener('click', function() {
+let csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
+$.ajaxSetup({
+        headers: {
+            'X-CSRFToken': csrftoken
+        }
+    });
+sad.addEventListener('click', function(event) {
+    if (event.target.dataset.hasOwnProperty('on')){
+        if (event.target.dataset.on ==='sad'){
+            alert('dataset.on')
+        }}
     alert('SAD');
-    });
-sad.addEventListener('click', function() {
-    alert('ok');
-    });
+    let data = {123:'gvh'};
+    $.ajax({
+        url: '',
+        method: 'POST',
+        data: data,
+        success: function (text) {
+            console.log('__ok__');
+            console.log(text);
+            alert('success');
+        },
+        error: function (text) {
+            console.log('__1__');
+            console.log(text);
+            alert('error');
+        },
+    })
+
+    }, true);
