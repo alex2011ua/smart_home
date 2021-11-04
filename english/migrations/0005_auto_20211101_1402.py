bad2bf71c7d139d916e357ebd815652193aefc35
Aleksii Kovalenko <alex2011ua@gmail.com>
2021-11-04 12:49:42 +0200

    add level 3

--- english/migrations/0005_auto_20211101_1402.py
+++ english/migrations/0005_auto_20211101_1402.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='wordparams',
-            name='english',
+            name='level_2',
             field=models.BooleanField(default=False),
         ),
         migrations.AddField(
