6ce0d35da1156eea3d2e61b1b837a11fe28af6a0
Aleksii Kovalenko <alex2011ua@gmail.com>
2021-11-16 13:47:10 +0200

    add users

--- house/settings.py
+++ house/settings.py
@@ -143,7 +143,7 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/english/'
 
