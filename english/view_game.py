2b6eeb61d51d078245c897abe0dfebae4734abdc
AlexUA_home <alex2011ua@gmail.com>
2021-12-06 22:51:09 +0200

    ss

--- english/view_game.py
+++ english/view_game.py
@@ -14,16 +14,21 @@ class GenrundGame(LoginRequiredMixin, View):
     def post(request):
         content = []
         words_all = Words.objects.filter(lesson=88)
-        for _word in words_all:
-            word, buttons = _word.english.split('-')
-            answer, russian = _word.russian.split('-')
-            buttons = buttons.split(',')
-            d_word = {
-                'word': word,
-                'buttons': buttons,
-                'answer': answer,
-                'russian': russian,
-                'id': _word.id,
-                      }
-            content.append(d_word)
+        try:
+            for _word in words_all:
+                word, buttons = _word.english.split('-',1)
+                answer, russian = _word.russian.split('-',1)
+                buttons = buttons.split(',')
+                d_word = {
+                    'word': word,
+                    'buttons': buttons,
+                    'answer': answer,
+                    'russian': russian,
+                    'id': _word.id,
+                          }
+                content.append(d_word)
+        except Exception as arr:
+            print(arr.text)
+            print (_word)
+
         return JsonResponse(content, safe=False)
