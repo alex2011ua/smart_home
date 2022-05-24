9a666cd0b2facc292e006f86c56b57476d0171f0
Aleksii Kovalenko <alex2011ua@gmail.com>
2022-05-24 15:19:33 +0300

    lvl 4

--- house/core/models.py
+++ house/core/models.py
@@ -6,7 +6,7 @@ class Setting(models.Model):
     controller_name = models.CharField(max_length=40, unique=True)
     label = models.CharField(max_length=100)
     value = models.IntegerField(default=20)
-    date = models.DateTimeField(null=True)
+    date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return str(self.label)
@@ -25,7 +25,7 @@ class Weather(models.Model):
 
 class Logs(models.Model):
     date_log = models.DateTimeField()
-    status = models.CharField(max_length=50, null=True)
+    status = models.CharField(max_length=50, null=True, blank=True)
     title_log = models.CharField(max_length=50)
     description_log = models.CharField(max_length=150)
 
@@ -36,27 +36,27 @@ class Logs(models.Model):
 class DHT_MQ(models.Model):
     date_t_h = models.DateTimeField()
 
-    temp_street = models.FloatField(null=True)
-    humidity_street = models.FloatField(null=True)
+    temp_street = models.FloatField(null=True, blank=True)
+    humidity_street = models.FloatField(null=True, blank=True)
 
-    temp_voda = models.FloatField(null=True)
-    humidity_voda = models.FloatField(null=True)
+    temp_voda = models.FloatField(null=True, blank=True)
+    humidity_voda = models.FloatField(null=True, blank=True)
 
-    temp_gaz = models.FloatField(null=True)
-    humidity_gaz = models.FloatField(null=True)
+    temp_gaz = models.FloatField(null=True, blank=True)
+    humidity_gaz = models.FloatField(null=True, blank=True)
 
-    temp_teplica = models.FloatField(null=True)
-    humidity_teplica = models.FloatField(null=True)
+    temp_teplica = models.FloatField(null=True, blank=True)
+    humidity_teplica = models.FloatField(null=True, blank=True)
 
-    temp_room = models.FloatField(null=True)
-    humidity_room = models.IntegerField(null=True)
+    temp_room = models.FloatField(null=True, blank=True)
+    humidity_room = models.IntegerField(null=True, blank=True)
 
-    gaz_MQ4 = models.IntegerField(null=True)
-    gaz_MQ135 = models.IntegerField(null=True)
+    gaz_MQ4 = models.IntegerField(null=True, blank=True)
+    gaz_MQ135 = models.IntegerField(null=True, blank=True)
 
-    muve_kitchen = models.IntegerField(null=True)
-    myData = models.CharField(max_length=50, null=True)
-    ackData = models.CharField(max_length=50, null=True)
+    muve_kitchen = models.IntegerField(null=True, blank=True)
+    myData = models.CharField(max_length=50, null=True, blank=True)
+    ackData = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return str(self.date_t_h)
@@ -65,20 +65,20 @@ class DHT_MQ(models.Model):
 class Params(models.Model):
     date_t_h = models.DateTimeField()
 
-    poliv = models.IntegerField(null=True)
-    min_temp_teplica = models.IntegerField(null=True)
-    max_temp_teplica = models.IntegerField(null=True)
-    ping = models.FloatField(null=True)
-    download = models.FloatField(null=True)
-    upload = models.FloatField(null=True)
+    poliv = models.IntegerField(null=True, blank=True)
+    min_temp_teplica = models.IntegerField(null=True, blank=True)
+    max_temp_teplica = models.IntegerField(null=True, blank=True)
+    ping = models.FloatField(null=True, blank=True)
+    download = models.FloatField(null=True, blank=True)
+    upload = models.FloatField(null=True, blank=True)
 
 
 class Message(models.Model):
     date_message = models.DateTimeField()
     controller_name = models.CharField(max_length=40, unique=True)
-    label = models.CharField(max_length=100, null=True)
-    value_int = models.IntegerField(null=True)
-    value_float = models.FloatField(null=True)
+    label = models.CharField(max_length=100, null=True, blank=True)
+    value_int = models.IntegerField(null=True, blank=True)
+    value_float = models.FloatField(null=True, blank=True)
     state = models.BooleanField(default=False)
 
 
