7630c5479161a7af205eb21575112ec714a6bf29
Aleksii Kovalenko <alex2011ua@gmail.com>
2022-06-03 09:58:51 +0300

    add regular filtering poliv

--- house/core/tasks.py
+++ house/core/tasks.py
@@ -92,7 +92,7 @@ def weather_task():
     tomorrow.temp_max = six_day["max_temp"]
     tomorrow.snow = six_day["summ_snow_3_day"]
     tomorrow.save()
-
+    calend()
     print("weather_task end")
 
 
@@ -329,7 +329,7 @@ def bot_task_watering_analiz():
         poliv.value = water_time - sum_rain
         poliv.label = "включен"
     poliv.save()
-    calend()
+
 
 
 @cellery_app.task()
